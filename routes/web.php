5fbe20613150e5b14e231e0c60889cbe7b65f542
bilalassraf <bilal.0300.it@gmail.com>
2020-12-25 16:15:29 +0500

    first commit by bilal

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 Route::group(['middleware' => ['auth', 'user']], function(){
-
+      
 	Route::get('dashboard','FrontEndController@index')->name('dashboard');
 
 });
